3ac506bca6ef1c0eef0fab133e3227dcc902a53a
Regi Shehi <shehi.regi@gmail.com>
2020-03-16 12:54:00 +0100

    Add SignalR

--- client-app/src/app/models/activity.ts
+++ client-app/src/app/models/activity.ts
@@ -9,6 +9,16 @@ export interface IActivity {
 	isGoing: boolean;
 	isHost: boolean;
 	attendees: IAttendee[];
+	comments: IComment[];
+}
+
+export interface IComment {
+	id: string;
+	createdAt: Date;
+	body: string;
+	username: string;
+	displayName: string;
+	image: string;
 }
 
 export interface IActivityFormValues extends Partial<IActivity> {
@@ -29,7 +39,6 @@ export class ActivityFormValues implements IActivityFormValues {
 		if (init && init.date) {
 			init.time = init.date;
 		}
-
 		Object.assign(this, init);
 	}
 }
